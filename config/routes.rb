6a932a9348eb31e5496a9a376b943caaf25a19da
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-01-26 12:33:44 +0000

    [interruption]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get 'books/update'
   root to: "homes#top"
   devise_for :users
+
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/homes/about" => "homes#about", as: "about"
 end
